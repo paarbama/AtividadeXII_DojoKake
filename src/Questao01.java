29f911845f47caf52b45d6bd1ed200cdb23f205f
paarbama <paulomacedo10@hotmail.com.br>
2016-05-27 11:20:18 -0300

    Q01

--- src/Questao01.java
+++ src/Questao01.java
@@ -2,8 +2,43 @@
 public class Questao01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			preencherMatriz();
+
+	}
+
+	static void preencherMatriz() {
+
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Informe a matriz");
+		n = scanner.nextInt();
+		int[][] A = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if(i == j){
+					A[i][j] = 0;
+				}else{
+					if (i+j == n-1) {
+						A[i][j] = 0;
+					}else{
+						A[i][j] = 1;
+					}
+				}		
+			}			
+
+		}
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(A[i][j]+ "\t");
+
+			}
+			System.out.println("");
+		}
 
 	}
 
 }
+
